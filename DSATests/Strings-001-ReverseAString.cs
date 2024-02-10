4e23fbfd4f04923e3fd1bc8b62dfc411f60587c6
neel <neelmallepally@gmail.com>
2024-02-10 11:55:13 -0600

    Added Strings-001 and Strings-002

---
+++ DSATests/Strings-001-ReverseAString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSATests
+{
+    public class Strings_001_ReverseAString
+    {
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { "TOM", "MOT" };
+            yield return new object[] { null, null };
+            yield return new object[] { "", "" };
+            yield return new object[] { " ", " " };
+            yield return new object[] { "dom", "mod" };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(string input, string expected)
+        {
+            var actual = Reverse(input);
+            Assert.Equal(expected, actual);
+        }
+
+
+        private string Reverse(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+
+            var output = new StringBuilder();
+
+            for (int i = input.Length -1; i >= 0; i--)
+            {
+                output.Append(input[i]);
+            }
+
+
+            return output.ToString();
+        }
+    }
+}
---
+++ DSATests/Strings-002-CompareCharInTwoStrings.cs
@@ -0,0 +1,82 @@
+﻿namespace DSATests
+{
+
+    // PROBLEM: Given a source string and a target string, find if a character in source string is present in the target string,
+    // if it is present and it is in the same location in both source and target then return upper case.
+    // if it is present but in different location then return lower case
+    // if it is not present then return "-"
+    // EXAMPLE: source: spain, target: spare then output should be SPA--
+    // EXAMPLE: source: lame, target: male then output should be -AlE
+    // ASSUMPTIONS: 1. both source and target strings are of same length
+    //              2. character comparison is case insensitive
+    public class Strings_002_CompareCharInTwoStrings
+    {
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { "spain", "spare", "SPA--" };
+            yield return new object[] { "lame", "male", "lAmE" };
+            yield return new object[] { "void", "drop", "-o-d" };
+            yield return new object[] { "loomt", "croob", "-oO--" };
+            yield return new object[] { "loop", "void", "-Oo-" };
+            yield return new object[] { "", "void", "" };
+            yield return new object[] { null, "void", "" };
+            yield return new object[] { "toaster", "void", "" };
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(string source, string target, string expected)
+        {
+            var result = CompareTheStrings(source, target);
+
+            Assert.True(expected.Equals(result), $"Expected: {expected}, Actual: {result}");
+        }
+
+
+
+        private string CompareTheStrings(string source, string target)
+        {
+
+            if(string.IsNullOrWhiteSpace(source) || string.IsNullOrWhiteSpace(target)) 
+            { 
+                return string.Empty;
+            }
+
+            if (source.Length != target.Length)
+            {
+                return string.Empty;
+            }
+
+            int i = 0;
+            var output = "";
+            while(i < source.Length)
+            {
+                var s = source[i];
+                var t = target[i];
+
+                if(target.Contains(s) && s == t)
+                {
+                     output += Char.ToUpper(s);
+                }
+                else if (target.Contains(s) && s != t)
+                {
+                    output += Char.ToLower(s);
+                }
+                else
+                {
+                    output += "-";
+                }
+
+                i++;
+            }
+
+            return output;
+        }
+    }
+
+
+
+
+}
